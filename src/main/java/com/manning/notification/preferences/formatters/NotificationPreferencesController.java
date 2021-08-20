5480953a37eccace8aa8ff35f5a29cd63616a711
amitbasnak500 <79815917+amitbasnak500@users.noreply.github.com>
2021-08-20 14:27:50 -0500

    Create NotificationPreferencesController.java

---
+++ src/main/java/com/manning/notification/preferences/formatters/NotificationPreferencesController.java
@@ -0,0 +1,28 @@
+package com.manning.notification.preferences.formatters;
+
+import com.manning.notification.preferences.entities.NotificationPreferences;
+import com.manning.notification.preferences.model.NotificationPreferencesResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationPreferencesFormatter
+{
+    public NotificationPreferencesResponse formatNotificationResponse(NotificationPreferences notificationPreference) {
+        NotificationPreferencesResponse notificationPreferencesResponse=new NotificationPreferencesResponse();
+        if(notificationPreference!=null && notificationPreference.getId()!= null) {
+            notificationPreferencesResponse.setEmailPreferenceFlag(notificationPreference.isEmailpreferenceflag());
+            notificationPreferencesResponse.setSmsPreferenceFlag(notificationPreference.isSmspreferenceflag());
+            notificationPreferencesResponse.setEmailAddress(notificationPreference.getEmailaddress());
+            notificationPreferencesResponse.setPhoneNumber(notificationPreference.getPhonenumber());
+            notificationPreferencesResponse.setStatus("SUCCESS");
+            notificationPreferencesResponse.setStatusDescription("Notification Received Successfully");
+        }
+        else
+        {
+            notificationPreferencesResponse.setStatus("ERROR");
+            notificationPreferencesResponse.setStatusDescription("Notification Failed");
+        }
+        return  notificationPreferencesResponse;
+
+    }
+}
